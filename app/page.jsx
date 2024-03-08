e30c6e048fe446b73ab8f133ec76ede119001b30
aryan <aryankarma29@gmail.com>
2024-03-08 13:39:38 +0000

    firstPageFinal

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./page.module.scss";
-import TakeInput from "./components/inputFile";
+import DragDrop from "./components/Drag&Drop";
+// import Card, { DraggableCardWrapper } from './components/Drag&Drop'
 
 export default function Home() {
+
   return (
     <div className={styles.parentContainer}>
       
@@ -17,30 +19,11 @@ export default function Home() {
           <h1>Are you exhausted with lengthy documents?</h1>
           <h1>ENIGMA can help you!</h1>
         </div>
-        
-        <div className={styles.card}>
 
-          <div className={styles.dragAndDrop}>
-            <img className={styles.uploadLogo} src="/img/uploadLogo.png" alt="" />
-            <h1>Drag & Drop files</h1>
-          </div>
-
-          <div className={styles.or}>
-            <p style={{color:"#999", fontSize:"27px", fontWeight:"200"}}>--------------<span style={{fontWeight:"500"}}>&nbsp; or &nbsp; </span>--------------</p> 
-          </div>
-
-          <div className={styles.selectFileContainer}>
-            <label for="upload-file">
-                <span className={styles.selectFileBtn}>Select File</span>
-                <input type="file" id="upload-file" accept=".pdf,.doc,.docx" style={{display:"none"}}/>
-            </label>
-          </div>
-
-        </div>
-
+        <DragDrop/> {/* componenet containing drag & drop functionality*/}
 
       <Link className={styles.link} href="/conversatoin">
-          <button id={styles.resetbutton}>Analysis Results</button>
+          <button id={styles.analysisbutton}>Analysis Results</button>
       </Link>
 
       </main>
